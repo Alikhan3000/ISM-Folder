976f469ce05e8a39b845ffeec0febd933d9d3fe5
Alikhan <fixator2004@gmail.com>
2025-01-26 16:09:19 -0500

    Task 3 - Financial Transactions.

--- cc_1.js
+++ cc_1.js
@@ -8,14 +8,25 @@ console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
 
 //task 2
-let productName = "Vinegar"
-const productPrice = 3.99
-var isAvailable = true
+let productName = "Vinegar";
+const productPrice = 3.99;
+var isAvailable = true;
 
 console.log(productName, typeof productName);
 console.log(productPrice, typeof productPrice);
 console.log(isAvailable, typeof isAvailable);
 
 //task 3
+let accountBalance = 500;
 
+accountBalance -= 450;
+console.log(accountBalance);
 
+accountBalance *= 10;
+console.log(accountBalance);
+
+accountBalance /=500;
+console.log(accountBalance);
+
+accountBalance += 3.99;
+console.log(accountBalance)
