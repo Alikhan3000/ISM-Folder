13190dc91fcc2f74845c0b947b8640cd168a85e9
Alikhan <fixator2004@gmail.com>
2025-01-26 15:49:00 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,21 @@
-let employeeName = "Ali"
-const employeeID = 2040
-var isActive = true
+//task 1
+let employeeName = "Ali";
+const employeeID = 2040;
+var isActive = true;
 
-console.log(employeeName, typeof employeeName)
-console.log(employeeID, typeof employeeID)
-console.log(isActive, typeof isActive)
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
+
+//task 2
+let productName = "Vinegar"
+const productPrice = 3.99
+var isAvailable = true
+
+console.log(productName, typeof productName);
+console.log(productPrice, typeof productPrice);
+console.log(isAvailable, typeof isAvailable);
+
+//task 3
+
+
