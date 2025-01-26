75e1126659956a81bd4aeefff7fac9f0a84a1554
Alikhan <fixator2004@gmail.com>
2025-01-26 15:36:33 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "Ali"
+const employeeID = 2040
+var isActive = true
+
+console.log(employeeName, typeof employeeName)
+console.log(employeeID, typeof employeeID)
+console.log(isActive, typeof isActive)
